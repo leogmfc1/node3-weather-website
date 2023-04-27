b8cd2a117850a82a3ec8c5d024a8b341c644d704
Leonardo Cavalcanti <leogmfc10@gmail.com>
2023-04-26 20:49:07 -1000

    Added new features to weather description

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,18 +1,27 @@
 const request = require("request");
 
 const forecast = (lat, long, callback) => {
-    
-    const url = "http://api.weatherstack.com/current?access_key=eb43b21692a35fff42281452046d1c5a&query=" + lat + "," + long
+    const url =
+        "http://api.weatherstack.com/current?access_key=eb43b21692a35fff42281452046d1c5a&query=" +
+        lat +
+        "," +
+        long;
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback("Unable to connect to weather service!", undefined);
         } else if (body.error) {
-            callback("Unable to find the location. Try another search.",undefined);
+            callback(
+                "Unable to find the location. Try another search.",
+                undefined
+            );
         } else {
-            callback(undefined, `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degrees out.`);
+            callback(
+                undefined,
+                `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degrees out. The wind speed is ${body.current.wind_speed}, and the humidity is ${body.current.humidity}.`
+            );
         }
     });
 };
 
-module.exports = forecast
+module.exports = forecast;
